fc0ff150a4d04167ba1f2051d4e60bc87031664b
JacobsM14 <marcel.esplu@gmail.com>
2024-05-18 21:54:12 +0200

    -

--- src/routes/mainRoutine.js
+++ src/routes/mainRoutine.js
@@ -3,84 +3,85 @@ const router = express.Router();
 const conn = require("../database/connection");
 
 // SHOW mainRoutine
-
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
-    const rows = await conn.query("SELECT * FROM mainRoutine");
-    console.log(rows);
-    return res.json(rows[0]);
+    const [rows, fields] = await conn.query("SELECT * FROM mainRoutine");
+    if (rows.length) {
+      return res.status(200).json(rows);
+    } else {
+      return res.status(404).json({ error: "Not found" });
+    }
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ error });
   }
 });
 
 // SHOW mainRoutine BY USER ID
-// router.get("/:id", async (req, res, next) => {
-//   try {
-//     const [rows, fields] = await conn.query(
-//       "SELECT * FROM mainRoutine WHERE fk_id_user = ?",
-//       [req.params.id]
-//     );
-//     if (rows.length) {
-//       return res.status(200).json(rows);
-//     } else {
-//       return res.status(404).json({ error: "Not found" });
-//     }
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// });
+router.get("/:id", async (req, res, next) => {
+  try {
+    const [rows, fields] = await conn.query(
+      "SELECT * FROM mainRoutine WHERE fk_id_user = ?",
+      [req.params.id]
+    );
+    if (rows.length) {
+      return res.status(200).json(rows);
+    } else {
+      return res.status(404).json({ error: "Not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 
-// // SHOW mainRoutine BY ROUTINE ID AND USER ID
-// router.get("/:id/:id_user", async (req, res, next) => {
-//   try {
-//     const [rows, fields] = await conn.query(
-//       "SELECT * FROM mainRoutine WHERE fk_id_user = ? AND fk_id_routine = ?",
-//       [req.params.id_user, req.params.id]
-//     );
-//     if (rows.length) {
-//       return res.status(200).json(rows);
-//     } else {
-//       return res.status(404).json({ error: "Not found" });
-//     }
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// });
+// SHOW mainRoutine BY ROUTINE ID AND USER ID
+router.get("/:id/:id_user", async (req, res, next) => {
+  try {
+    const [rows, fields] = await conn.query(
+      "SELECT * FROM mainRoutine WHERE fk_id_user = ? AND fk_id_routine = ?",
+      [req.params.id_user, req.params.id]
+    );
+    if (rows.length) {
+      return res.status(200).json(rows);
+    } else {
+      return res.status(404).json({ error: "Not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 
-// // POST mainRoutine
-// router.post("/:id", async (req, res) => {
-//   const { fk_id_user } = req.body;
+// POST mainRoutine
+router.post("/:id", async (req, res) => {
+  const { fk_id_user } = req.body;
 
-//   try {
-//     const [rows, fields] = await conn.query(
-//       "INSERT INTO mainRoutine (fk_id_user, fk_id_routine) VALUES (?, ?)",
-//       [fk_id_user, req.params.id]
-//     );
+  try {
+    const [rows, fields] = await conn.query(
+      "INSERT INTO mainRoutine (fk_id_user, fk_id_routine) VALUES (?, ?)",
+      [fk_id_user, req.params.id]
+    );
 
-//     const [newRoutine] = await conn.query(
-//       "SELECT * FROM mainRoutine WHERE pk_id_mainroutine = ?",
-//       [rows.insertId]
-//     );
+    const [newRoutine] = await conn.query(
+      "SELECT * FROM mainRoutine WHERE pk_id_mainroutine = ?",
+      [rows.insertId]
+    );
 
-//     return res.status(201).json(newRoutine[0]);
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// });
+    return res.status(201).json(newRoutine[0]);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 
-// // DELETE mainRoutine BY USER ID
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const [rows, fields] = await conn.query(
-//       "DELETE FROM mainRoutine WHERE fk_id_user = ?",
-//       [req.params.id]
-//     );
-//     return res.status(200).json(rows);
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// });
+// DELETE mainRoutine BY USER ID
+router.delete("/:id", async (req, res) => {
+  try {
+    const [rows, fields] = await conn.query(
+      "DELETE FROM mainRoutine WHERE fk_id_user = ?",
+      [req.params.id]
+    );
+    return res.status(200).json(rows);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
