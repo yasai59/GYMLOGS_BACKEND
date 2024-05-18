4fadc3d651abb00af1547e8d9296a968fe2a10eb
JacobsM14 <marcel.esplu@gmail.com>
2024-05-18 20:56:20 +0200

    -

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const routes = express.Router()
 
-// routes.use('/typeExercise', require('./typesexercise'));
+routes.use('/typeExercise', require('./typesexercise'));
 
-// routes.use('/category', require('./category'));
+routes.use('/category', require('./category'));
 
-// routes.use('/exercise', require('./exercise'));
+routes.use('/exercise', require('./exercise'));
 
-// routes.use('/users', require('./users'));
+routes.use('/useres', require('./users'));
 
-// routes.use('/routine', require('./routine'));
+routes.use('/routine', require('./routine'));
 
-// routes.use('/mainRoutine', require('./mainRoutine'));
+routes.use('/mainRoutine', require('./mainRoutine'));
 
-// routes.use('/sessions', require('./sessions'));
+routes.use('/sessions', require('./sessions'));
 
-// routes.use('/sessions_exercise', require('./sessions_exercise'));
+routes.use('/sessions_exercise', require('./sessions_exercise'));
 
-// routes.use('/calendar', require('./calendar'));
+routes.use('/calendar', require('./calendar'));
 
 module.exports = routes;
