fada0560adadadae816f0a505db86a28912c7b0a
JacobsM14 <marcel.esplu@gmail.com>
2024-05-20 11:38:18 +0200

    -

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -2,6 +2,35 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../database/connection");
 
+//GET ALL SESSIONS
+router.get("/", async (req, res) => {
+  try {
+    const [rows, fields] = await conn.query("SELECT * FROM calendar");
+    return res.status(200).json(rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+});
+
+//GET BY SESSION EXERCISE AND DAY
+router.get("/exercise/:id/:day", async (req, res) => {
+  try {
+    const rows = await conn.query(
+      "SELECT * FROM calendar WHERE fk_id_session_ex = ? AND day = ?",
+      [req.params.id, req.params.day]
+    );
+    if (rows.length) {
+      return res.status(200).json(rows);
+    } else {
+      return res.status(404).json({ error: "Calendar not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+});
+
 //GET BY SESSION ID
 router.get("/:id", async (req, res) => {
   try {
@@ -37,15 +66,15 @@ router.post("/", async (req, res) => {
         "INSERT INTO calendar (serie, weight, repetitions, duration, day, fk_id_session_ex) VALUES (?, ?, ?, ?, ?, ?)",
         [serie, weight, repetitions, duration, day, fk_id_session_ex]
       );
-        return res.status(201).json({
-          pk_id_calendar: rows2.insertId,
-          serie: serie,
-          weight: weight,
-          repetitions: repetitions,
-          duration: duration,
-          day: day,
-          fk_id_session_ex: fk_id_session_ex,
-        });
+      return res.status(201).json({
+        pk_id_calendar: rows2.insertId,
+        serie: serie,
+        weight: weight,
+        repetitions: repetitions,
+        duration: duration,
+        day: day,
+        fk_id_session_ex: fk_id_session_ex,
+      });
     }
   } catch (error) {
     console.log(error);
