2cf5750d4eebecf4851c0a83e75b7d3627d8e68a
JacobsM14 <marcel.esplu@gmail.com>
2024-05-20 19:46:36 +0200

    -

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -39,7 +39,17 @@ router.get("/exercise/:id", async (req, res) => {
       [req.params.id]
     );
     if (rows.length) {
-      return res.status(200).json(rows);
+      return res.status(200).json(
+        {
+          pk_id_calendar: rows.pk_id_calendar,
+          serie: rows.serie,
+          weight: rows.weight,
+          repetitions: rows.repetitions,
+          duration: rows.duration,
+          day: rows.day,
+          fk_id_session_ex: rows.fk_id_session_ex,
+        }
+      );
     } else {
       return res.status(404).json({ error: "Calendar not found" });
     }
